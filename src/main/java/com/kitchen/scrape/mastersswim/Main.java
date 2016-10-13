4125c438225afbaa5a9b5e5f8addf2d68aa43eaf
Kevin Chen <kevinmichaelchen@gmail.com>
2016-10-12 23:29:42 -0400

    write csv

--- src/main/java/com/kitchen/scrape/mastersswim/Main.java
+++ src/main/java/com/kitchen/scrape/mastersswim/Main.java
@@ -21,17 +21,53 @@ public class Main
 {
 	private static final String BASE_URL = "http://www.usms.org/comp/tt/toptenlist.php";
 
-	public static void main( String[] args )
+	public static void main( String[] args ) throws IOException
 	{
 		File output = new File( "output.txt" );
+		output.delete();
+		output.createNewFile();
+
 		File errorLog = new File( "error.log" );
+		errorLog.delete();
+		errorLog.createNewFile();
+
+		writeHeaders( output );
+
 		scrape( "1971", "1", "M", "9", output, errorLog );
+//		scrapeAll( output, errorLog );
 	}
 
-	private static void scrapeAll( File output, File errorLog )
+	private static void writeHeaders( File output )
+	{
+		StringBuilder sb = new StringBuilder();
+		sb.append( "rank" )
+			.append( "," )
+			.append( "name" )
+			.append( "," )
+			.append( "age" )
+			.append( "," )
+			.append( "club" )
+			.append( "," )
+			.append( "lmsc" )
+			.append( "," )
+			.append( "time" )
+			.append( "," )
+			.append( "year" )
+			.append( "," )
+			.append( "sex" )
+			.append( "," )
+			.append( "event" )
+			.append( "," )
+			.append( "distance" )
+			.append( "\n" );
+		String headers = sb.toString();
+		tryWrite( headers, output );
+	}
+
+	private static void scrapeAll( File output, File errorLog ) throws IOException
 	{
 		List<String> years = IntStream.range( 1971, 2017 ).mapToObj( Integer::toString ).collect( Collectors.toList() );
-		List<String> courseIDs = Arrays.asList( "1", "2", "3" );
+		List<String> courseIDs = Arrays.asList( "1" );
 		List<String> sexes = Arrays.asList( "M", "W" );
 		List<String> ageGroupIDs = IntStream.range( 1, 18 ).mapToObj( Integer::toString ).collect( Collectors.toList() );
 
@@ -81,6 +117,7 @@ public class Main
 				{
 					Element table = tables.get( i );
 					String header = headers.get( i );
+					header = header == null ? "" : header.trim();
 					System.out.println( header );
 					for ( Element row : table.select( "tr" ) )
 					{
@@ -93,18 +130,64 @@ public class Main
 
 						if ( tds.size() != 6 )
 						{
-							String errorMessage = String.format( "Expected 6 columns, found %d", tds.size() );
-							FileUtils.writeStringToFile( errorLog, errorMessage, StandardCharsets.UTF_8 );
+							String errorMessage = String.format( "Expected 6 columns, found %d\n", tds.size() );
+							tryWrite( errorMessage, errorLog );
 						}
 						else
 						{
-							tds.forEach( td -> System.out.print( td.text() + " | " ) );
+							for ( int tdi = 0; tdi < tds.size(); tdi++ )
+							{
+								Element td = tds.get( tdi );
+								System.out.print( td.text() + " | " );
+								String tdText = td.text();
+								tdText = tdText == null ? "" : tdText.trim();
+								tdText = tdText.replace( "&nbsp;", "" ).replace( "\u00a0", "" );
+								if ( tdi != tds.size() - 1 )
+								{
+									tdText += ",";
+								}
+								tryWrite( tdText, output );
+							}
+							// year, sex, stroke, distance
+							tryWrite( ",", output );
+							tryWrite( year, output );
+							tryWrite( ",", output );
+							tryWrite( sex, output );
+							tryWrite( ",", output );
+							tryWrite( header, output );
+							tryWrite( ",", output );
+							tryWrite( tryParseDistanceFromStroke( header ), output );
+							tryWrite( "\n", output );
 						}
 						System.out.println( "\n" );
 					}
 					System.out.println( "\n\n" );
 				}
 			}
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private static String tryParseDistanceFromStroke( String stroke )
+	{
+		try
+		{
+			return stroke.split( " " )[0].trim();
+		}
+		catch ( Exception e )
+		{
+			return "";
+		}
+	}
+
+	private static void tryWrite( String s, File file )
+	{
+		try
+		{
+			FileUtils.writeStringToFile( file, s, StandardCharsets.UTF_8, true );
 		}
 		catch ( IOException e )
 		{
